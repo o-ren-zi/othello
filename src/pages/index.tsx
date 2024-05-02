d0b506f2675bac083265f9c25011607af9a2f7e0
o-ren-zi <s1f102400369@iniad.org>
2024-05-02 19:25:45 +0900

    オセロ502

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,19 +10,37 @@ const directions = [
   [-1, 1],
   [0, 1],
 ];
-
+// function newvalue(x: number, y: number) {
+//   return x >= 0 && x < 8 && y >= 0 && y < 8;
+// }
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
+  const [memoly, setmemoly] = useState(0);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 0, 0],
+    [0, 0, 0, 1, 2, 2, 0, 0],
+    [0, 0, 0, 2, 1, 1, 2, 2],
+    [0, 0, 0, 0, 0, 0, 1, 2],
+    [0, 0, 0, 0, 0, 0, 2, 2],
+    [0, 0, 0, 0, 0, 0, 1, 1],
   ]);
+  // function samecolor(x: number, y: number) {
+  //   return (board[y][x] = turnColor);
+  // }
+
+  // function othercolor(x: number, y: number) {
+  //   return (board[y][x] = 3 - turnColor);
+  // }
+
+  // function zerocolor(x: number, y: number) {
+  //   return (board[y][x] = 0);
+  // }
+
+  // const dx = directions[0];
+  // const dy = directions[1];
+
   const clickHander = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
@@ -40,8 +58,18 @@ const Home = () => {
               newBoard[y][x] = turnColor;
               setBoard(newBoard);
               setTurnColor(3 - turnColor);
+              
+
+              setmemoly(i);
+              for (let a = 1; a < i; a++) {
+                newBoard[y + direction[0] * a][x + direction[1] * a] = turnColor;
+                setBoard(newBoard);
+                setTurnColor(3 - turnColor);
+              }
+
               break;
             } else {
+              console.log(x, y);
               break;
             }
 
